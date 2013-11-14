eba8d2615c8a03f353a544cf282b889fdf0a9630
nolure <nolure@vip.qq.com>
2013-11-14 11:55:18 +0800

    1

--- js/nojs/module/calendar.js
+++ js/nojs/module/calendar.js
@@ -1,1 +1,1 @@
-/*Thu Nov 14 2013 11:05:11 GMT+0800 (中国标准时间)-http://nolure.github.io/nojs.docs*/define("nojs/module/calendar",[],function(){});
+/*2013/11/14-http://nolure.github.io/nojs.docs*/define("nojs/module/calendar",[],function(){});
